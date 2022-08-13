5f9a5e0e3e23755dab2161be05d14b9fe9a68fa7
zhangyaning <1176164611@qq.com>
2022-08-13 19:21:16 +0800

    test

--- src/main/java/com/zyn/test/test.java
+++ src/main/java/com/zyn/test/test.java
@@ -7,5 +7,7 @@ package com.zyn.test;
 public class test {
     public static void main(String[] args) {
         System.out.println("11111");
+        System.out.println("22222");
+        System.out.println("33333");
     }
 }
