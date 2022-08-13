f49e01223cd4b5cf1e0c0014c8f2f50ee64d63d3
zhangyaning <1176164611@qq.com>
2022-08-13 19:14:27 +0800

    idea_test

---
+++ src/main/java/com/zyn/test/test.java
@@ -0,0 +1,11 @@
+package com.zyn.test;
+
+/**
+ * @author zyn
+ * @date 2022-08-13-19:12
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("11111");
+    }
+}
