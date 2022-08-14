baa07db58f6c62912d21d4f32b1c8db062419012
zzzzzzz14 <94155667+zzzzzzz14@users.noreply.github.com>
2022-08-14 19:13:04 +0800

    Update test.java

--- src/main/java/com/zyn/test/test.java
+++ src/main/java/com/zyn/test/test.java
@@ -10,5 +10,6 @@ public class test {
         System.out.println("22222");
         System.out.println("33333");
         System.out.println("44444");
+        System.out.println("55555");
     }
 }
